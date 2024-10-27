a53702c08f52f6661f9a438c266eb3854ab56688
Matthew Davies <m.davies.work@gmail.com>
2024-10-27 17:36:43 +0900

    Tick method introduced

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while pygame.get_init():
         for event in pygame.event.get():
              if event.type == pygame.QUIT:
                 return
         screen.fill(000000, rect=None, special_flags=0)
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
+        print(dt)
 if __name__ == "__main__":
     main()
